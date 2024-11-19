2e952ae1d92869b657496ba331cf78148b840314
justlikesh <lpsac@naver.com>
2024-11-19 11:59:09 +0900

    first init

---
+++ src/main/java/HappyPerson/FIT/ISLAND/FitIslandApplication.java
@@ -0,0 +1,13 @@
+package HappyPerson.FIT.ISLAND;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitIslandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitIslandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/HappyPerson/FIT/ISLAND/FitIslandApplicationTests.java
@@ -0,0 +1,13 @@
+package HappyPerson.FIT.ISLAND;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitIslandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
