59eeb03b83c46816d84f4f33403aa31b5f0a243e
sean <tmdgus717@naver.com>
2024-12-12 21:05:26 +0900

    heatlthCheckController 임시 추가

---
+++ src/main/java/happyperson/fitisland/common/healthcheck/HealthCheckController.java
@@ -0,0 +1,14 @@
+package happyperson.fitisland.common.healthcheck;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+        return new ResponseEntity<>("OK", HttpStatus.OK);
+    }
+}
